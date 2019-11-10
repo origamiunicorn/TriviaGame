b4c98cf67061364fe6bf59c0c3b8cec647a67c54
origami unicorn <rvwright@gmail.com>
2019-11-09 17:06:38 -0800

    Changed on click in buildQuestion to register for .questionDive instead of doc. No longer seeing multiple registrations of div clicks.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,84 +43,113 @@ $(document).ready(function () {
 
     $("#displayTrivia").hide();
 
-    // Create a function
-    // Let this function display a question and answers from the object array
-    // ... but only if there is no question displayed currently...
-    // ... and only display one question at a time.
-
     var questionNum = 0;
-
-    var correctAnswer;
+    let countDown = 3000;
 
     function populateQuestion() {
         var displayTrivia = $("#displayTrivia");
-        var newDiv = $("<div>").addClass("triviaQuestions");
-        var wrongAnswerDiv = $("<div>").addClass("wrongAnswerTrivia");
-        var rightAnswerDiv = $("<div>").addClass("rightAnswerTrivia");
-
-        var question = false;
-
-        // while (!question) {
-        //     console.log("Question number in while loop: " + questionNum);
-        //     question = true;
+        // var newDiv = $("<div>").addClass("triviaQuestions");
+        // var wrongAnswerDiv = $("<div>").addClass("wrongAnswerTrivia");
+        // var rightAnswerDiv = $("<div>").addClass("rightAnswerTrivia");
 
         for (let i = 0; i < triviaQuestions.length; i++) {
+            let myTime = i * countDown;
+            clearInterval(myVar);
             setTimeout(function () {
                 console.log(triviaQuestions[i]);
-                // Put question on DOM
-                newDiv.html($("<p>" + triviaQuestions[i].question + "</p>"));
-                displayTrivia.append(newDiv);
+
+                // Builds questions
+                const currentObject = triviaQuestions[questionNum];
+                const question = currentObject.question;
+                const answers = currentObject.answers;
+
+                buildQuestion(question, answers, displayTrivia);
 
                 if (i === triviaQuestions.length - 1) {
                     setTimeout(function () {
-                        console.log("Game over!")
+                        // console.log("Game over!")
+                        gameOver(displayTrivia);
                     }, 3000)
                 }
-            }, 3000 * i);
+                questionNum++;
+            }, myTime);
         };
-
-        // for (var i = 0; i < triviaQuestions.length; i++) {
-        //     console.log("Question number in FOR loop: " + questionNum);
-        //     console.log(triviaQuestions[i]);
-        //     if (questionNum < triviaQuestions.length) {
-        //         correctAnswer = triviaQuestions[i + questionNum].correct;
-        //         console.log("The current correct answer is: " + correctAnswer);
-        //         newDiv.html($("<p>" + triviaQuestions[i + questionNum].question + "</p>"));
-        //         newDiv.append($("<div>").addClass("questionDiv").val("a").text(triviaQuestions[i + questionNum].answers.a));
-        //         newDiv.append($("<div>").addClass("questionDiv").val("b").text(triviaQuestions[i + questionNum].answers.b));
-        //         newDiv.append($("<div>").addClass("questionDiv").val("c").text(triviaQuestions[i + questionNum].answers.c));
-        //         newDiv.append($("<div>").addClass("questionDiv").text(triviaQuestions[i + questionNum].answers.d).val("d"));
-        //         wrongAnswerDiv.html($("<p>").text(triviaQuestions[i + questionNum].text));
-        //         wrongAnswerDiv.append($("<img>").attr("src", triviaQuestions[i + questionNum].gif));
-        //         rightAnswerDiv.html($("<img>").attr("src", triviaQuestions[i + questionNum].gif));
-        //         displayTrivia.append(newDiv);
-        //         displayTrivia.append(rightAnswerDiv);
-        //         rightAnswerDiv.hide();
-        //         displayTrivia.append(wrongAnswerDiv);
-        //         wrongAnswerDiv.hide();
-        //         console.log("current question index number: " + i);
-        //         i++
-        //         console.log("next question index number: " + i);
-        //         console.log(question);
-        //         questionNum++;
-        //         console.log("Current question number: " + questionNum);
-        //         return;
-        //     } else {
-        //         stop();
-        //         displayTrivia.append($("<div>").text("You completed the quiz."));
-        //     }
-        // }
-        //    
     }
 
-    // Create a timer that counts down from 30 seconds.
+    function buildQuestion(qus, ans, targetDiv) {
+        // Build my question and answers from my object array values
+        targetDiv.empty();
+        var newDiv = $("<div>");
+        newDiv.html($("<p>" + qus + "</p>"));
+        newDiv.append($("<div>").addClass("questionDiv").val("a").text(ans.a));
+        newDiv.append($("<div>").addClass("questionDiv").val("b").text(ans.b));
+        newDiv.append($("<div>").addClass("questionDiv").val("c").text(ans.c));
+        newDiv.append($("<div>").addClass("questionDiv").val("d").text(ans.d));
+        targetDiv.append(newDiv);
+        $(".questionDiv").on("click", function () {
+            playerSelect = $(this).val();
+            console.log(playerSelect);
+            console.log(questionNum);
+        })
+        console.log(newDiv);
+    }
 
-    var number = 5;
-    var intervalId;
-    var intervalIsRunning = false;
+    var seconds = 5;
+    var myVar = setInterval(function () {
+        myTimer();
+    }, 3000);
 
-    var intervalIdQuestions;
-    var intervalBetweenQuestions = false;
+    function resetTimer() {
+        seconds = 5;
+    }
+
+    function myTimer() {
+        $("#countdownTimer").html = seconds < 10 ? `00:0${seconds--}` : `00:${seconds--}`;
+        if (seconds === -1) {
+            resetTimer();
+            populateQuestion();
+        }
+    }
+
+    function gameOver(displayDiv) {
+        displayDiv.html($("<p>").addClass("gameOver").text("Game over!"));
+        calculateScore();
+    }
+
+    function calculateScore() {
+        console.log("Calculating Score Later");
+    }
+
+    // Create a timer that counts down from n seconds.
+
+    // var number = 5;
+
+    // var intervalId;
+    // var intervalIsRunning = false;
+
+
+    // function run() {
+    //     if (!intervalIsRunning) {
+    //         intervalId = setInterval(decrement, 1000); // Set intervalID to an interval passing decfrement function, and 1000 ms
+    //         intervalIsRunning = true; // When the function is run, set intervalIsRunning to true.
+    //     }
+    // }
+
+    // function decrement() {
+    //     number--;
+    //     $("#countdownTimer").html(number);
+
+    //     if (number === 0) {
+    //         stop();
+    //         alert("Time Up!");
+    //     }
+    // }
+
+    // function stop() {
+
+    //     clearInterval(intervalId);
+    //     intervalIsRunning = false; // Now when this is stopped, you must set interval to false.
+    // }
 
     $("#timerDisplay").hide(); // Start with the timer hidden.
 
@@ -131,75 +160,94 @@ $(document).ready(function () {
 
         $("#startTimer").hide(); // Hide the timer.
         $("#displayTrivia").show(); // Show the trivia box.
-        populateQuestion(); // Populate a question.
-        var playerSelect; // This is a variable for the player's answer!
-
-        $(".questionDiv").click(function () {
-            playerSelect = $(this).val();
-            console.log(playerSelect);
-            determineWinLose(playerSelect); // Put the player's answer into the win/lose, see if they won/lost.
-        })
-    }
-
-    var win = 0;
-    var lose = 0;
-    var timeOut = 0;
-
-    function determineWinLose(choice) {
-        if (choice === correctAnswer) {
-            $(".rightAnswerTrivia").show().prepend($("<h2>Correct!</h2>"));
-            $(".triviaQuestions").hide();
-            stop();
-            question = false;
-            win++;
-            console.log("# Wins:" + win);
-            getNextQuestion();
-            return false;
-        } else {
-            $(".wrongAnswerTrivia").show().prepend($("<h2>Not Quite!</h2>"));
-            $(".triviaQuestions").hide();
-            stop();
-            question = false;
-            lose++;
-            console.log("# Lose: " + lose);
-            getNextQuestion();
-            return false;
-        }
-    }
-
-    function getNextQuestion() {
-        if (!intervalBetweenQuestions) {
-            intervalIdQuestions = setInterval(nextQuestion, 5000); // Run function after 5 seconds
-            intervalBetweenQuestions = true;
-        }
-    }
-
-    function nextQuestion() { // Will need some way of checking that we have asked all questions to display the Final Results and allow for a reset of the game without page reload
-        $("#displayTrivia").empty();
-        number = 5;
-        startTimer();
-        clearInterval(intervalIdQuestions);
-        intervalBetweenQuestions = false;
-    }
-
-    function decrement() {
         $("#timerDisplay").show();
-        number--;
-        $("#countdownTimer").html(number);
-        if (number === 0) {
-            stop();
-            question = false;
-            $(".wrongAnswerTrivia").show().prepend($("<h2>Out of Time!</h2>"));
-            $(".triviaQuestions").hide();
-            timeOut++;
-            console.log("# Time Outs: " + timeOut);
-            getNextQuestion();
-        }
+        populateQuestion(); // Populate a question.
+        // var playerSelect; // This is a variable for the player's answer!
+
+        // $(".questionDiv").click(function () {
+        //     playerSelect = $(this).val();
+        //     console.log(playerSelect);
+        //     determineWinLose(playerSelect); // Put the player's answer into the win/lose, see if they won/lost.
+        // })
     }
 
-    function stop() {
+    // var win = 0;
+    // var lose = 0;
+    // var timeOut = 0;
 
-        clearInterval(intervalId);
-        intervalIsRunning = false;
-    }
+    // function determineWinLose(choice) {
+    //     if (choice === correctAnswer) {
+    //         $(".rightAnswerTrivia").show().prepend($("<h2>Correct!</h2>"));
+    //         $(".triviaQuestions").hide();
+    //         stop();
+    //         question = false;
+    //         win++;
+    //         console.log("# Wins:" + win);
+    //         getNextQuestion();
+    //         return false;
+    //     } else {
+    //         $(".wrongAnswerTrivia").show().prepend($("<h2>Not Quite!</h2>"));
+    //         $(".triviaQuestions").hide();
+    //         stop();
+    //         question = false;
+    //         lose++;
+    //         console.log("# Lose: " + lose);
+    //         getNextQuestion();
+    //         return false;
+    //     }
+    // }
+
+    // function getNextQuestion() {
+    //     if (!intervalBetweenQuestions) {
+    //         intervalIdQuestions = setInterval(nextQuestion, 5000); // Run function after 5 seconds
+    //         intervalBetweenQuestions = true;
+    //     }
+    // }
+
+    // function nextQuestion() { // Will need some way of checking that we have asked all questions to display the Final Results and allow for a reset of the game without page reload
+    //     $("#displayTrivia").empty();
+    //     number = 5;
+    //     startTimer();
+    //     clearInterval(intervalIdQuestions);
+    //     intervalBetweenQuestions = false;
+    // }
+
+    // function decrement() {
+    //     $("#timerDisplay").show();
+    //     number--;
+    //     $("#countdownTimer").html(number);
+    //     if (number === 0) {
+    //         stop();
+    //         question = false;
+    //         $(".wrongAnswerTrivia").show().prepend($("<h2>Out of Time!</h2>"));
+    //         $(".triviaQuestions").hide();
+    //         timeOut++;
+    //         console.log("# Time Outs: " + timeOut);
+    //         getNextQuestion();
+    //     }
+    // }
+
+    // function stop() {
+
+    //     clearInterval(intervalId);
+    //     intervalIsRunning = false;
+    // }
 });
+
+
+
+        //         wrongAnswerDiv.html($("<p>").text(triviaQuestions[i + questionNum].text));
+        //         wrongAnswerDiv.append($("<img>").attr("src", triviaQuestions[i + questionNum].gif));
+        //         rightAnswerDiv.html($("<img>").attr("src", triviaQuestions[i + questionNum].gif));
+        //         displayTrivia.append(newDiv);
+        //         displayTrivia.append(rightAnswerDiv);
+        //         rightAnswerDiv.hide();
+        //         displayTrivia.append(wrongAnswerDiv);
+        //         wrongAnswerDiv.hide();
+
+        //     } else {
+        //         stop();
+        //         displayTrivia.append($("<div>").text("You completed the quiz."));
+        //     }
+        // }
+        // 
